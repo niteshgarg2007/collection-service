6dedf408879ccb5eeac2edb182967a161652b7dc
nitesh.garg <nitesh.garg@0fddf1f0-dabb-7140-b7d0-a464e74b6d4c>
2023-04-21 14:26:54 +0000

    Corrected JWT Filter

--- ITS/collection-service/src/main/java/com/hcl/igovern/config/FilterConfig.java
+++ ITS/collection-service/src/main/java/com/hcl/igovern/config/FilterConfig.java
@@ -6,24 +6,23 @@ import org.springframework.context.annotation.Configuration;
 
 import jakarta.servlet.Filter;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
 
-	@Bean
-	public FilterRegistrationBean<Filter>  jwtFilter() {
-		FilterRegistrationBean<Filter> filter= new FilterRegistrationBean<>();
-        filter.setFilter(new JwtFilter());
-        
-        //filter.addUrlPatterns("/its/apis/overpayment/addoverpayment");
-        filter.addUrlPatterns("/claimant/service2");
-        filter.addUrlPatterns("/claimant/add");
-        filter.addUrlPatterns("/claimant/all");
-        filter.addUrlPatterns("/api/claimant/all");
-        filter.addUrlPatterns("/api/employer/update");
-        filter.addUrlPatterns("/api/employer/find/**");
-        filter.addUrlPatterns("/api/employer/update");
-        filter.addUrlPatterns("/api/employer/delete");
-        filter.addUrlPatterns("/api/employer/addEmp");
-        return filter;
-	}
+	/*
+	 * @Bean public FilterRegistrationBean<Filter> jwtFilter() {
+	 * FilterRegistrationBean<Filter> filter= new FilterRegistrationBean<>();
+	 * filter.setFilter(new JwtFilter());
+	 * 
+	 * filter.addUrlPatterns("/its/apis/overpayment/*");
+	 * filter.addUrlPatterns("/claimant/service2");
+	 * filter.addUrlPatterns("/claimant/add");
+	 * filter.addUrlPatterns("/claimant/all");
+	 * filter.addUrlPatterns("/api/claimant/all");
+	 * filter.addUrlPatterns("/api/employer/update");
+	 * filter.addUrlPatterns("/api/employer/find/**");
+	 * filter.addUrlPatterns("/api/employer/update");
+	 * filter.addUrlPatterns("/api/employer/delete");
+	 * filter.addUrlPatterns("/api/employer/addEmp"); return filter; }
+	 */
 }
