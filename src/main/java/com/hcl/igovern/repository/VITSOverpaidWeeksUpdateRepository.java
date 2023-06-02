e15be8a39deab77573cd4cae65d50525f05238f3
thakur-p <thakur-p@0fddf1f0-dabb-7140-b7d0-a464e74b6d4c>
2023-06-02 07:43:58 +0000

    Overpayment And Recovery Search

--- src/main/java/com/hcl/igovern/repository/VITSOverpaidWeeksUpdateRepository.java
+++ src/main/java/com/hcl/igovern/repository/VITSOverpaidWeeksUpdateRepository.java
@@ -11,10 +11,8 @@ import com.hcl.igovern.entity.VITSOverpaidWeeksUpdateEO;
 @Repository
 public interface VITSOverpaidWeeksUpdateRepository extends JpaRepository<VITSOverpaidWeeksUpdateEO, Long> {
 
-	List<VITSOverpaidWeeksUpdateEO> findByVictimBadActorXrefIdAndClmIdOrderByCbwkBweDt(Long victimBadActorXrefId, Long inputClaimId);
-
-	List<VITSOverpaidWeeksUpdateEO> findByVictimBadActorXrefIdAndClmIdAndOvpIdOrderByCbwkBweDt(Long victimBadActorXrefId, Long inputClaimId, Long ovpId);
+	List<VITSOverpaidWeeksUpdateEO> findByVictimBadActorXrefIdAndClmIdAndIsCancelledAndOvpIdOrderByCbwkBweDt(Long victimBadActorXrefId, Long inputClaimId, String isCancelled, Long ovpId);
 	
-	@Query(value = "SELECT * FROM V_ITS_OVERPAID_WEEKS_UPDATE WHERE VICTIM_BAD_ACTOR_XREF_ID=?1 AND CLM_ID=?2 AND (OVP_ID=?3 OR OVP_ID IS NULL)", nativeQuery = true)
-	List<VITSOverpaidWeeksUpdateEO> getOverpaidWeeksForUpdate(Long victimBadActorXrefId, Long inputClaimId, Long ovpId);
+	@Query(value = "SELECT * FROM V_ITS_OVERPAID_WEEKS_UPDATE WHERE VICTIM_BAD_ACTOR_XREF_ID=?1 AND CLM_ID=?2 AND IS_CANCELLED=?3 AND (OVP_ID=?4 OR OVP_ID IS NULL)", nativeQuery = true)
+	List<VITSOverpaidWeeksUpdateEO> getOverpaidWeeksForUpdate(Long victimBadActorXrefId, Long inputClaimId, String isCancelled, Long ovpId);
 }
