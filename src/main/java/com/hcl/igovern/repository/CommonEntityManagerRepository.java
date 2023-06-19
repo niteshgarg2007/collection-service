b91a6d17cd75279af0b8e681ec8fb11cb337ddf1
thakur-p <thakur-p@0fddf1f0-dabb-7140-b7d0-a464e74b6d4c>
2023-06-19 10:05:37 +0000

    Prosecution List - Add - View - Update

--- src/main/java/com/hcl/igovern/repository/CommonEntityManagerRepository.java
+++ src/main/java/com/hcl/igovern/repository/CommonEntityManagerRepository.java
@@ -1,6 +1,7 @@
 package com.hcl.igovern.repository;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +19,7 @@ import com.hcl.igovern.entity.VITSOvpSummaryEO;
 import com.hcl.igovern.entity.VITSRecoverySummaryEO;
 import com.hcl.igovern.exception.BusinessException;
 import com.hcl.igovern.util.DateUtil;
+import com.hcl.igovern.vo.ItsProsecutionsOverpaymentXrefVO;
 import com.hcl.igovern.vo.SearchBadActorDataVO;
 
 import jakarta.persistence.EntityManager;
@@ -222,5 +224,175 @@ public class CommonEntityManagerRepository {
 			throw new BusinessException(ERR_CODE, "Exception in CommonEntityManagerRepository while calling getCostCodeMapping method" +e.getMessage());
 		}
 		return costCodeList;
+	}
+
+	public List<ItsProsecutionsOverpaymentXrefVO> getVITSProsecutionUpdateByXrefList(Long victimBadActorXrefId, Long prosId) {
+		List<?> itsOvpSummaryVOListTemp = null;
+		List<ItsProsecutionsOverpaymentXrefVO> vITSOverpaidWeeksExistingList = new ArrayList<>();
+		ItsProsecutionsOverpaymentXrefVO prosOvpXrefVO = null;
+		String query=null;
+        String sql=null;
+		try {
+			query = "SELECT DISTINCT V.OVP_ID,V.DATE_CREATED,V.OVP_TOTAL,V.RECOVERY_AMOUNT,V.OVP_PENALTY,V.OVP_INTEREST,V.OVP_COC,"
+					+ " V.OVPSTS_CD,V.OVPDIS_CD,V.BAD_ACTOR_SSN,V.PRTY_TAX_ID,V.OVP_BALANCE,"
+					+ " X.PROS_ID,X.PROS_OVP_XREF_ID,X.RESI_AMT FROM V_ITS_OVP_SUMMARY V"
+					+ " LEFT JOIN ITS_PROSECUTIONS_OVERPAYMENT_XREF X On V.OVP_ID=X.OVP_ID AND X.END_DT IS NULL AND X.PROS_ID=?"
+					+ " WHERE VICTIM_BAD_ACTOR_XREF_ID=?";
+			sql = String.format(query);
+			itsOvpSummaryVOListTemp = entityManager.createNativeQuery(sql).setParameter(1, prosId).setParameter(2, victimBadActorXrefId).getResultList();
+			if(itsOvpSummaryVOListTemp!=null && !itsOvpSummaryVOListTemp.isEmpty()) {
+				for (int i = 0; i < itsOvpSummaryVOListTemp.size(); i++) {
+					prosOvpXrefVO = new ItsProsecutionsOverpaymentXrefVO();
+					Object[] val = (Object[])itsOvpSummaryVOListTemp.get(i);
+					
+					prosOvpXrefVO = setITSProsOvpXrefData(val,prosOvpXrefVO);
+					
+					vITSOverpaidWeeksExistingList.add(prosOvpXrefVO);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Exception in CommonEntityManagerRepository while calling getVITSProsecutionUpdateByXrefList method");
+			throw new BusinessException(ERR_CODE, "Exception in CommonEntityManagerRepository while calling getVITSProsecutionUpdateByXrefList method" +e.getMessage());
+		}
+		return vITSOverpaidWeeksExistingList;
+	}
+
+	public List<ItsProsecutionsOverpaymentXrefVO> getVITSProsecutionUpdateByBadActorList(Long badActorId, Long prosId) {
+		List<?> itsOvpSummaryVOListTemp = null;
+		List<ItsProsecutionsOverpaymentXrefVO> vITSOverpaidWeeksExistingList = new ArrayList<>();
+		ItsProsecutionsOverpaymentXrefVO prosOvpXrefVO = null;
+		String query=null;
+        String sql=null;
+		try {
+			query = "SELECT DISTINCT V.OVP_ID,V.DATE_CREATED,V.OVP_TOTAL,V.RECOVERY_AMOUNT,V.OVP_PENALTY,V.OVP_INTEREST,V.OVP_COC,"
+					+ " V.OVPSTS_CD,V.OVPDIS_CD,V.BAD_ACTOR_SSN,V.PRTY_TAX_ID,V.OVP_BALANCE,"
+					+ " X.PROS_ID,X.PROS_OVP_XREF_ID,X.RESI_AMT FROM V_ITS_OVP_SUMMARY V"
+					+ " LEFT JOIN ITS_PROSECUTIONS_OVERPAYMENT_XREF X On V.OVP_ID=X.OVP_ID AND X.END_DT IS NULL AND X.PROS_ID=?"
+					+ " WHERE BAD_ACTOR_ID=?";
+			sql = String.format(query);
+			itsOvpSummaryVOListTemp = entityManager.createNativeQuery(sql).setParameter(1, prosId).setParameter(2, badActorId).getResultList();
+			if(itsOvpSummaryVOListTemp!=null && !itsOvpSummaryVOListTemp.isEmpty()) {
+				for (int i = 0; i < itsOvpSummaryVOListTemp.size(); i++) {
+					prosOvpXrefVO = new ItsProsecutionsOverpaymentXrefVO();
+					Object[] val = (Object[])itsOvpSummaryVOListTemp.get(i);
+					
+					prosOvpXrefVO = setITSProsOvpXrefData(val,prosOvpXrefVO);
+					
+					vITSOverpaidWeeksExistingList.add(prosOvpXrefVO);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Exception in CommonEntityManagerRepository while calling getVITSProsecutionUpdateByBadActorList method");
+			throw new BusinessException(ERR_CODE, "Exception in CommonEntityManagerRepository while calling getVITSProsecutionUpdateByBadActorList method" +e.getMessage());
+		}
+		return vITSOverpaidWeeksExistingList;
+	}
+
+	private ItsProsecutionsOverpaymentXrefVO setITSProsOvpXrefData(Object[] val, ItsProsecutionsOverpaymentXrefVO prosOvpXrefVO) {
+		try {
+			if (val != null && val.length > 0 && val[0] != null && val[0] instanceof BigDecimal) {
+				Long ovpIdVal = Long.valueOf(((BigDecimal) val[0]).longValue());
+				prosOvpXrefVO.setOvpId(ovpIdVal);
+			}
+			
+			if (val != null && val.length > 0 && val[1] != null) {
+				Timestamp dateCreated = (Timestamp) val[1];
+				prosOvpXrefVO.setDateCreated(DateUtil.tsDateToStr(dateCreated));
+			}
+			
+			if (val != null && val.length > 0 && val[2] != null && val[2] instanceof BigDecimal) {
+				Double ovpTotal = Double.valueOf(((BigDecimal) val[2]).doubleValue());
+				prosOvpXrefVO.setOvpTotal(ovpTotal);
+			}
+			
+			if (val != null && val.length > 0 && val[3] != null && val[3] instanceof BigDecimal) {
+				Double recoveryAmount = Double.valueOf(((BigDecimal) val[3]).doubleValue());
+				prosOvpXrefVO.setRecoveryAmount(recoveryAmount);
+			}
+			
+			if (val != null && val.length > 0 && val[4] != null && val[4] instanceof BigDecimal) {
+				Double ovpPenalty = Double.valueOf(((BigDecimal) val[4]).doubleValue());
+				prosOvpXrefVO.setOvpPenalty(ovpPenalty);
+			}
+			
+			if (val != null && val.length > 0 && val[5] != null && val[5] instanceof BigDecimal) {
+				Double ovpInterest = Double.valueOf(((BigDecimal) val[5]).doubleValue());
+				prosOvpXrefVO.setOvpInterest(ovpInterest);
+			}
+			
+			if (val != null && val.length > 0 && val[6] != null && val[6] instanceof BigDecimal) {
+				Double ovpCoC = Double.valueOf(((BigDecimal) val[6]).doubleValue());
+				prosOvpXrefVO.setOvpCoc(ovpCoC);
+			}
+			
+			prosOvpXrefVO = setITSProsOvpXrefNextData(val,prosOvpXrefVO);
+			
+			
+			return prosOvpXrefVO;
+		} catch (Exception e) {
+			logger.error("Exception in CommonEntityManagerRepository while calling setITSProsOvpXrefData method");
+			throw new BusinessException(ERR_CODE, "Exception in CommonEntityManagerRepository while calling setITSProsOvpXrefData method" +e.getMessage());
+		}
+	}
+
+	private ItsProsecutionsOverpaymentXrefVO setITSProsOvpXrefNextData(Object[] val, ItsProsecutionsOverpaymentXrefVO prosOvpXrefVO) {
+		try {
+			if (val != null && val.length > 0 && val[7] != null) {
+				String ovpStsCd = (String) val[7];
+				prosOvpXrefVO.setOvpstsCd(ovpStsCd);
+			}
+			
+			if (val != null && val.length > 0 && val[8] != null) {
+				String ovpDisCd = (String) val[8];
+				prosOvpXrefVO.setOvpdisCd(ovpDisCd);
+			}
+			
+			if (val != null && val.length > 0 && val[9] != null) {
+				String badActorSsn = (String) val[9];
+				prosOvpXrefVO.setBadActorSsn(badActorSsn);
+			}
+			
+			if (val != null && val.length > 0 && val[10] != null) {
+				String prtyTaxId = (String) val[10];
+				prosOvpXrefVO.setPrtyTaxId(prtyTaxId);
+			}
+			
+			if (val != null && val.length > 0 && val[11] != null && val[11] instanceof BigDecimal) {
+				Double ovpBalance = Double.valueOf(((BigDecimal) val[11]).doubleValue());
+				prosOvpXrefVO.setOvpBalance(ovpBalance);
+			}
+			
+			setITSProsOvpXrefNext(val,prosOvpXrefVO);
+			
+			
+			return prosOvpXrefVO;
+		} catch (Exception e) {
+			logger.error("Exception in CommonEntityManagerRepository while calling setITSProsOvpXrefNextData method");
+			throw new BusinessException(ERR_CODE, "Exception in CommonEntityManagerRepository while calling setITSProsOvpXrefNextData method" +e.getMessage());
+		}
+	}
+
+	private ItsProsecutionsOverpaymentXrefVO setITSProsOvpXrefNext(Object[] val, ItsProsecutionsOverpaymentXrefVO prosOvpXrefVO) {
+		try {
+			if (val != null && val.length > 0 && val[12] != null && val[12] instanceof BigDecimal) {
+				Long prosIdVal = Long.valueOf(((BigDecimal) val[12]).longValue());
+				prosOvpXrefVO.setProsId(prosIdVal);
+			}
+			
+			if (val != null && val.length > 0 && val[13] != null && val[13] instanceof BigDecimal) {
+				Long prosOvpXrefIdVal = Long.valueOf(((BigDecimal) val[13]).longValue());
+				prosOvpXrefVO.setProsOvpXrefId(prosOvpXrefIdVal);
+			}
+			
+			if (val != null && val.length > 0 && val[14] != null && val[14] instanceof BigDecimal) {
+				Double restiAmount = Double.valueOf(((BigDecimal) val[14]).doubleValue());
+				prosOvpXrefVO.setRestiAmount(restiAmount);
+				prosOvpXrefVO.setResiAmt(restiAmount);
+			}
+			return prosOvpXrefVO;
+		} catch (Exception e) {
+			logger.error("Exception in CommonEntityManagerRepository while calling setITSProsOvpXrefNext method");
+			throw new BusinessException(ERR_CODE, "Exception in CommonEntityManagerRepository while calling setITSProsOvpXrefNext method" +e.getMessage());
+		}
 	}
 }
