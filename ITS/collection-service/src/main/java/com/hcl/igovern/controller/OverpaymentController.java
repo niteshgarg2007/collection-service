e7f3a51151fec8b0e67cf822003ea5258161907a
thakur-p <thakur-p@0fddf1f0-dabb-7140-b7d0-a464e74b6d4c>
2023-07-12 13:12:51 +0000

    removed cross origin

--- ITS/collection-service/src/main/java/com/hcl/igovern/controller/OverpaymentController.java
+++ ITS/collection-service/src/main/java/com/hcl/igovern/controller/OverpaymentController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +26,6 @@ import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/its/apis/overpayment")
-@CrossOrigin(origins = "*")
 @ResponseBody
 public class OverpaymentController {
 	
