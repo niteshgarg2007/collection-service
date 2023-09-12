751d9720bff6f84a364c71812998ee349d9a8612
thakur-p <thakur-p@0fddf1f0-dabb-7140-b7d0-a464e74b6d4c>
2023-09-12 07:40:10 +0000

    OWSAP Implementation

--- src/main/java/com/hcl/igovern/validation/InputLongValidator.java
+++ src/main/java/com/hcl/igovern/validation/InputLongValidator.java
@@ -2,8 +2,6 @@ package com.hcl.igovern.validation;
 
 import org.apache.commons.lang.StringUtils;
 
-import com.hcl.igovern.exception.BusinessException;
-
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
@@ -16,12 +14,6 @@ public class InputLongValidator implements ConstraintValidator<InputLongConstrai
 
 	@Override
 	public boolean isValid(String inputField, ConstraintValidatorContext cxt) {
-		boolean validInputField = false;
-		try {
-			validInputField = StringUtils.isNumeric(inputField);
-		} catch (Exception e) {
-			throw new BusinessException("ERR_CODE", "Invalid input received.");
-		}
-		return validInputField;
+		return StringUtils.isNumeric(inputField);
 	}
 }
