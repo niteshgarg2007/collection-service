22bc47bb4e90beec1e1c9992fb7e96d37fc36f0d
thakur-p <thakur-p@0fddf1f0-dabb-7140-b7d0-a464e74b6d4c>
2023-09-13 07:10:26 +0000

    Selenium Overpayment Summary

--- ITS/collection-service/src/main/java/com/hcl/igovern/repository/VITSOverpaidWeeksUpdateRepository.java
+++ ITS/collection-service/src/main/java/com/hcl/igovern/repository/VITSOverpaidWeeksUpdateRepository.java
@@ -13,6 +13,6 @@ public interface VITSOverpaidWeeksUpdateRepository extends JpaRepository<VITSOve
 
 	List<VITSOverpaidWeeksUpdateEO> findByVictimBadActorXrefIdAndClmIdAndIsCancelledAndOvpIdOrderByCbwkBweDt(Long victimBadActorXrefId, Long inputClaimId, String isCancelled, Long ovpId);
 	
-	@Query(value = "SELECT * FROM V_ITS_OVERPAID_WEEKS_UPDATE WHERE VICTIM_BAD_ACTOR_XREF_ID=?1 AND CLM_ID=?2 AND IS_CANCELLED=?3 AND (OVP_ID=?4 OR OVP_ID IS NULL)", nativeQuery = true)
+	@Query(value = "SELECT * FROM V_ITS_OVERPAID_WEEKS_UPDATE WHERE VICTIM_BAD_ACTOR_XREF_ID=?1 AND CLM_ID=?2 AND IS_CANCELLED=?3 AND (OVP_ID=?4 OR OVP_ID IS NULL) ORDER BY CBWK_BWE_DT", nativeQuery = true)
 	List<VITSOverpaidWeeksUpdateEO> getOverpaidWeeksForUpdate(Long victimBadActorXrefId, Long inputClaimId, String isCancelled, Long ovpId);
 }
